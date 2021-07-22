140b689c80053349d44a6437b39eead667460095
Suraj N <surajn@mactores.com>
2021-07-23 01:05:22 +0530

    Updated metrics

--- hbase_metrics.py
+++ hbase_metrics.py
@@ -26,6 +26,7 @@ class hbase_metrics:
     def fetch_and_push_metrics(self, port):
         print("Pushing Metrics")
         for metric in fetch_metrics("http://localhost:" + str(port)):
+            print(metric['metric'])
             if str(metric['metric']) in self.list_metrics:
                 print("Pushing METRIC:" + str(metric['metric']) + ": " + str(metric['value']))
                 statsd.gauge(metric['metric'], metric['value'],["{}:{}".format(k, v) for k, v in metric.get('tags', {}).items()])
